2f34916ab8ee2288c346be60e8722c6faea1383a
TahleeJ <tahlee.jaynes@gmail.com>
2021-10-09 13:46:12 -0400

    Add stopwatch functionality

--- Media Warden Code/background.js
+++ Media Warden Code/background.js
@@ -1,3 +1,6 @@
+// import { Stopwatch } from "./stopwatch";
+self.importScripts("./stopwatch.js");
+
 const media_blacklist_sites = ["facebook.com", "instagram.com", "reddit.com", "twitter.com", "youtube.com"];
 let media_blacklist_active_tabs = new Map();
 
@@ -14,13 +17,17 @@ chrome.tabs.onCreated.addListener((tab) =>{
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.status = "complete") {
         if (changeInfo.url != null) {
-            // Check if the current tab is newly created or not
+            // Check if the current tab is for a blacklisted site and newly created or not
             if (!media_blacklist_active_tabs.has(tab.id)) {
                 console.log(`Tab has been updated: ${tab.url}`);
 
                 // Check if the tab's url is one for one of the blacklisted social media sites
                 media_blacklist_sites.forEach(url => {
                     if (tab.url.includes(url)) {
+                        if (media_blacklist_active_tabs.size == 0) {
+                            startStopwatch();
+                        }
+
                         // Add tab to "blacklisted tabs" collection if it is on a blacklisted site
                         media_blacklist_active_tabs.set(tab.id, {"original_url": url});
                         console.log(`You have opened a tab for ${url}`);
@@ -50,6 +57,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                     // Remove tab from "blacklisted tabs" collection
                     media_blacklist_active_tabs.delete(tab.id);
                     console.log("This tab has left all blacklisted sites.");
+
+                    if (media_blacklist_active_tabs.size == 0) {
+                        console.log("All active blacklisted tabs in this session have been navigated away from.");
+                        console.log(stopStopwatch());
+                    }
                 }
             }
         } 
