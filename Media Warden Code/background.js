e869c23a440c53916cfcb2b613a50b1137bc9d9e
TahleeJ <tahlee.jaynes@gmail.com>
2021-10-09 11:42:37 -0400

    Add individual tab tracking and monitoring

--- Media Warden Code/background.js
+++ Media Warden Code/background.js
@@ -1,4 +1,5 @@
-const media_blacklist = ["facebook.com", "instagram.com", "reddit.com", "twitter.com"];
+const media_blacklist_sites = ["facebook.com", "instagram.com", "reddit.com", "twitter.com", "youtube.com"];
+let media_blacklist_active_tabs = new Map();
 
 // Listen for new tabs being created
 chrome.tabs.onCreated.addListener((tab) =>{
@@ -13,16 +14,44 @@ chrome.tabs.onCreated.addListener((tab) =>{
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.status = "complete") {
         if (changeInfo.url != null) {
-            console.log(`Tab has been updated: ${tab.url}`);
+            // Check if the current tab is newly created or not
+            if (!media_blacklist_active_tabs.has(tab.id)) {
+                console.log(`Tab has been updated: ${tab.url}`);
 
-            media_blacklist.forEach(url => {
-                if (tab.url.includes(url)) {
-                    console.log("This website is not for you.");
-                    setTimeout(() => {
-                        chrome.tabs.remove(tab.id);
-                    }, 3000);
+                // Check if the tab's url is one for one of the blacklisted social media sites
+                media_blacklist_sites.forEach(url => {
+                    if (tab.url.includes(url)) {
+                        // Add tab to "blacklisted tabs" collection if it is on a blacklisted site
+                        media_blacklist_active_tabs.set(tab.id, {"original_url": url});
+                        console.log(`You have opened a tab for ${url}`);
+                        // setTimeout(() => {
+                        //     chrome.tabs.remove(tab.id);
+                        // }, 3000);
+                    }
+                });
+            } else if (!changeInfo.url.includes(media_blacklist_active_tabs.get(tab.id).original_url)) {
+                // Handle the tab's url changing, the "pseudo-session" for the tab
+                console.log(`You have left ${media_blacklist_active_tabs.get(tab.id).original_url}`);
+
+                let is_blacklist = false;
+
+                // Check if the new url is still for a blacklisted site
+                media_blacklist_sites.forEach(url => {
+                    if (changeInfo.url.includes(url)) {
+                        is_blacklist = true;
+                        // Change the tab's "original" url to the current url
+                        media_blacklist_active_tabs.get(tab.id).original_url = url;
+                        console.log(`You have moved to new blacklisted site: ${url}`);
+                    }
+                })
+
+                // Handle the tab's new url not being a part of the blacklisted site collection
+                if (!is_blacklist) {
+                    // Remove tab from "blacklisted tabs" collection
+                    media_blacklist_active_tabs.delete(tab.id);
+                    console.log("This tab has left all blacklisted sites.");
                 }
-            });
-        }
+            }
+        } 
     }
 });
