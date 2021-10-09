5091a6e1b21bdd00827a4b33acfc85eeaa5b5660
kdean31 <kdean31@gatech.edu>
2021-10-09 19:08:41 -0400

    Alarm functionality implemented to add style changes to restricted webpages after set time

--- Tutorial Extension/popup.js
+++ Tutorial Extension/popup.js
@@ -6,23 +6,23 @@ chrome.storage.sync.get("color", ({ color }) => {
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
+//changeColor.addEventListener()
+//const tabId = getTabId();
 changeColor.addEventListener("click", async () => {
+  //  const tabId = getTabId();
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+    console.log("Button test");
+    //const tabId = getTabId();
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: setPageBackgroundColor, //change color
+      function: backgroundColor, //change color
     });
   });
   
   // The body of this function will be executed as a content script inside the
   // current page
   function setPageBackgroundColor() { //alter this
-    // chrome.storage.sync.get("color", ({ color }) => {
-    //   document.body.style.backgroundColor = color;
-    // });
-    const addCSS = s => document.head.appendChild(document.createElement("style")).innerHTML=s;
 
-    addCSS("html { -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); filter: grayscale(100%); </style>");
+    const addCSS = s => document.body.appendChild(document.createElement("style")).innerHTML=s;
+    addCSS("html { -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); filter: invert(75%); filter: grayscale(10%); </style>");
   }
-
